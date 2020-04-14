335d45a5967796fd6e7de8110ba6da7c96ee4887
Aaron Brewer <spaceribs@gmail.com>
2020-04-14 07:50:50 -0400

    Add onchange test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone, ChangeDetectorRef } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'zonejs-webext-issue';
+export class AppComponent implements OnInit {
+  title = "zonejs-webext-issue";
+  number = 0;
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    browser.storage.onChanged.addListener((changes) => {
+      this.number += 1;
+      console.log((window as any).Zone.current);
+      console.log(NgZone.isInAngularZone());
+    });
+
+    setTimeout(() => {
+      console.log((window as any).Zone.current);
+      browser.storage.local.set({ test: this.number.toString() });
+    }, 1000);
+  }
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,6 @@ import "zone.js/dist/zone"; // Included with Angular CLI.
  * APPLICATION IMPORTS
  */
 import browser from "webextension-polyfill";
+import "zone.js/dist/zone-patch-web-ext";
+
+(window as any).Zone[(window as any).Zone.__symbol__("webext")](browser);
